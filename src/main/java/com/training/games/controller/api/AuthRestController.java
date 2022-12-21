af3eeb316ae4223306a97521940a7ca8b1457671
sauvageb <sauvageboris.pro@gmail.com>
2022-12-21 15:40:14 +0100

    authController: catch exceptions around authenticate method

--- src/main/java/com/training/games/controller/api/AuthRestController.java
+++ src/main/java/com/training/games/controller/api/AuthRestController.java
@@ -3,6 +3,7 @@ package com.training.games.controller.api;
 import com.training.games.controller.dto.SigninRequest;
 import com.training.games.controller.dto.SignupRequest;
 import com.training.games.controller.exception.UserAlreadyExistException;
+import com.training.games.repository.entity.User;
 import com.training.games.security.jwt.JwtResponse;
 import com.training.games.security.jwt.JwtUtils;
 import com.training.games.service.UserService;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -40,27 +43,37 @@ public class AuthRestController {
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<?> signin(@RequestBody SigninRequest dto) {
+    public ResponseEntity<JwtResponse> signin(@RequestBody SigninRequest dto) throws Exception {
         // UsernamePasswordAuthenticationToken : implementation of the Authentication interface which specifies that the user wants to authenticate using a username and password.
         UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(dto.getUsername(), dto.getPassword());
         // Filter Authentification for username passed in SigninRequest
-        Authentication authentication = authenticationManager.authenticate(auth);
 
-        // The SecurityContextHolder is where Spring Security stores the details of who is authenticated.
-        // Spring Security does not care how the SecurityContextHolder is populated.
-        // If it contains a value, it is used as the currently authenticated user.
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        Authentication authentication = null;
+        try {
+            authentication = authenticationManager.authenticate(auth);
 
-        // Generate JSON Web Token (JWT) (RFC 7519)
-        String generatedToken = jwtUtils.generateJwt(authentication);
+            // The SecurityContextHolder is where Spring Security stores the details of who is authenticated.
+            // Spring Security does not care how the SecurityContextHolder is populated.
+            // If it contains a value, it is used as the currently authenticated user.
+            SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        // TODO : return username from Authentication SecurityContextHolder
-        String username = null;
-        JwtResponse jwtResponse = new JwtResponse(username, generatedToken);
+            // Generate JSON Web Token (JWT) (RFC 7519)
+            String generatedToken = jwtUtils.generateJwt(authentication);
 
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(jwtResponse);
+            // Get username from Authentication SecurityContextHolder
+            User connectedUser = (User) authentication.getPrincipal();
+            JwtResponse jwtResponse = new JwtResponse(connectedUser.getUsername(), generatedToken);
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(jwtResponse);
+
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+
     }
 
 }
