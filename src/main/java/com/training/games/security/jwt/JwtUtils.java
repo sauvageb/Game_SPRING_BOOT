64d25cedb0c316ec66209fc75c3d6124500aed18
sauvageb <sauvageboris.pro@gmail.com>
2022-12-21 18:16:47 +0100

    jjwt : upgrade dependancy to 0.11.5 version

--- src/main/java/com/training/games/security/jwt/JwtUtils.java
+++ src/main/java/com/training/games/security/jwt/JwtUtils.java
@@ -1,11 +1,16 @@
 package com.training.games.security.jwt;
 
 import com.training.games.repository.entity.User;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
@@ -18,21 +23,23 @@ public class JwtUtils {
 
     public String generateJwt(Authentication authentication) {
         User user = (User) authentication.getPrincipal();
-        return Jwts
-                .builder()
+
+        return Jwts.builder()
                 .setSubject(user.getUsername())
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(new Date().getTime() + this.expirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .signWith(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
                 .compact();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+                    .build().parseClaimsJws(token);
             return true;
-        } catch (SignatureException e) {
-            System.err.println("Invalid JWT signature: {} " + e.getMessage());
+//        } catch (SignatureException e) {
+//            System.err.println("Invalid JWT signature: {} " + e.getMessage());
         } catch (MalformedJwtException e) {
             System.err.println("Invalid JWT token: {} " + e.getMessage());
         } catch (ExpiredJwtException e) {
@@ -48,8 +55,9 @@ public class JwtUtils {
 
     public String getUsernameFromToken(String token) {
         return Jwts
-                .parser()
-                .setSigningKey(jwtSecret)
+                .parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8)))
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
