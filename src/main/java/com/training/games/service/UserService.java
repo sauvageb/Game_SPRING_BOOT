1e99e40eede5d201c971052311796de4fa90cff5
sauvageb <sauvageboris.pro@gmail.com>
2022-12-22 09:24:44 +0100

    feat(userService): add role during signup

--- src/main/java/com/training/games/service/UserService.java
+++ src/main/java/com/training/games/service/UserService.java
@@ -2,12 +2,18 @@ package com.training.games.service;
 
 import com.training.games.controller.dto.SignupRequest;
 import com.training.games.controller.exception.UserAlreadyExistException;
+import com.training.games.repository.RoleRepository;
 import com.training.games.repository.UserRepository;
+import com.training.games.repository.entity.Role;
+import com.training.games.repository.entity.RoleEnum;
 import com.training.games.repository.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,6 +21,8 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
     @Autowired
     private PasswordEncoder encoder;
 
@@ -26,6 +34,10 @@ public class UserService {
             throw new UserAlreadyExistException(dto.getUsername());
         }else {
             User newUser = new User(dto.getUsername(), encoder.encode(dto.getPassword()));
+            // Define role
+            Role roleUser = roleRepository.findByName(RoleEnum.ROLE_USER);
+            List<Role> roleList = Arrays.asList(roleUser);
+            newUser.setRoleList(roleList);
             userRepository.save(newUser);
         }
 
